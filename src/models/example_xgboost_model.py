b49bb33113e4e44faff97f93bc84983796d741dd
paulaoak <pc2222@ic.ac.uk>
2023-09-20 20:14:43 +0100

    add xgboost model as class

--- src/models/example_xgboost_model.py
+++ src/models/example_xgboost_model.py
@@ -1,27 +1,26 @@
-import pandas as pd
 from sklearn.model_selection import train_test_split
 import xgboost as xgb
 from sklearn.metrics import accuracy_score, classification_report
 
-# Assuming 'X' is your feature matrix and 'y' is your target variable
-# X should be a DataFrame and y should be a DataFrame with binary encoding for labels
+class MultiLabelXGBoostClassifier:
+    def __init__(self, objective='binary:logistic', random_state=42):
+        self.objective = objective
+        self.random_state = random_state
+        self.model = None
 
-# Split the data into a training set (80%) and a testing set (20%)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    def train(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=self.random_state)
+        self.model = xgb.XGBClassifier(objective=self.objective, random_state=self.random_state)
+        self.model.fit(X_train, y_train)
+        y_pred = self.model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f'Accuracy: {accuracy*100:.2f}%')
+        report = classification_report(y_test, y_pred)
+        print(f'Classification Report:\n{report}')
 
-# Initialize the XGBoost classifier
-xgb_model = xgb.XGBClassifier(objective='binary:logistic', random_state=42)
+    def predict(self, X):
+        if self.model is None:
+            raise ValueError("Model has not been trained. Please call 'train' method first.")
+        return self.model.predict(X)
 
-# Train the model on the training data
-xgb_model.fit(X_train, y_train)
 
-# Predict the labels for the test set
-y_pred = xgb_model.predict(X_test)
-
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy*100:.2f}%')
-
-# Generate a detailed classification report
-report = classification_report(y_test, y_pred)
-print(f'Classification Report:\n{report}')
