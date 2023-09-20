e2cab79efc9974a0f9fc5c7ed835b10240d03c49
Marcus September <marcusGH@users.noreply.github.com>
2023-09-20 18:26:57 +0100

    Elaborate example files

---
+++ src/feature-design/feature_design1.py
@@ -0,0 +1,8 @@
+'''
+Contains methods that loads data from data/derived/ (preprocessed data),
+adds new informative features to the dataset, and writes back to data/derived/ with
+a different name.
+'''
+
+def add_summary_statistics():
+    pass
--- src/preprocessing/data_formatting.py
+++ src/preprocessing/data_formatting.py
@@ -1,6 +1,19 @@
 '''
-Take the data in raw/ and write to data/derived/ with suitable numeric
-data
+Take the data in raw/ and write to data/derived/ with suitable numeric data
 '''
+import pandas as pd
 
-# TODO
+def fill_missing_data(df):
+    df
+
+def winsorize_outliers(df):
+    return df
+
+
+if __name__ == '__main__':
+    df = pd.read_csv(os.path.join("data", "raw", "my_data.csv"))
+
+    df = fill_missing_data(df)
+    df = winsorize_outliers(df)
+
+    df.to_csv(os.path.join("data", "derived", "my_preprocesed_data.csv"))
