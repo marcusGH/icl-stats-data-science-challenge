85c4f66cc660a6176e58979151750baaab7f28b9
Marcus September <marcusGH@users.noreply.github.com>
2023-09-21 12:07:03 +0100

    Add submission code

--- src/preprocessing/data_formatting.py
+++ src/preprocessing/data_formatting.py
@@ -32,6 +32,30 @@ def winsorize_outliers(df):
     return df
 
 
+def fill_remaining_cols(df):
+    extended_cols = ['AnB', 'AnC', 'AnD', 'BnC', 'BnD', 'CnD',
+                     'AnBnC', 'AnBnD', 'AnCnD', 'BnCnD', 'AnBnCnD']
+    for i in range(len(df['A'])):
+        for c in extended_cols:
+            fill_val = True
+            for l in ['A', 'B', 'C', 'D']:
+                if l in c:
+                    fill_val = fill_val and (df.at[i, l] == 1)
+            # modify the cell
+            df.at[i, c] = int(fill_val)
+
+def submit_dataframe(df, submit_path):
+    df = df.reset_index()
+    assert len(df.columns) == 29
+    df.to_csv(submit_path, index=True, quotechar='"', sep=' ')
+
+    # replace index columns with nothing
+    with open(submit_path) as f:
+        lines = f.readlines()
+    lines[0] = lines[0][1:]
+    with open(submit_path, "w") as f:
+        f.writelines(lines)
+
 if __name__ == '__main__':
     df = pd.read_csv(os.path.join("data", "raw", "my_data.csv"))
 
