b54b0ee6dffbbc577855d64663350c883e668ef2
Marcus September <marcusGH@users.noreply.github.com>
2023-09-21 10:27:45 +0100

    Add preprocessed data

--- src/preprocessing/data_formatting.py
+++ src/preprocessing/data_formatting.py
@@ -4,6 +4,22 @@ Take the data in raw/ and write to data/derived/ with suitable numeric data
 import pandas as pd
 import os
 
+
+def get_preprocessed_data(data_dir):
+    """
+    :param data_dir: path to the input directory
+    :return:
+    """
+    train_path = os.path.join(data_dir, "raw", "train.csv")
+    preprocessed_path = os.path.join(data_dir, "derived", "train-preprocessed.csv")
+
+    with open(train_path, "rt") as fin:
+        with open(preprocessed_path, "wt") as fout:
+            for line in fin:
+                fout.write(line.replace('\\"', "'"))
+
+    return pd.read_csv(preprocessed_path, sep=r'\s+', quotechar='"')
+
 def fill_missing_data(df):
     return df
 
