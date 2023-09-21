627b946328f5cd1691820010b46eb1f729b292b6
Marcus September <marcusGH@users.noreply.github.com>
2023-09-21 14:39:11 +0100

    Add competition metric

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -1,7 +1,20 @@
 '''
 Implement whatever competition metric comes up tomorrow (Thursday)
 '''
+from sklearn.metrics import balanced_accuracy_score
 
-def competition_metric(y_pred, y_true):
-    pass
+def competition_metric(df_pred, df_true):
+    extended_cols = ['AnB', 'AnC', 'AnD', 'BnC', 'BnD', 'CnD',
+                     'AnBnC', 'AnBnD', 'AnCnD', 'BnCnD', 'AnBnCnD']
+    target_cols = ['A', 'B', 'C', 'D']
+
+    # find the weights for each of the columns
+    final_score = 0.
+    for c in target_cols:
+        w = 0.5 / len(target_cols)
+        final_score += w * balanced_accuracy_score(df_true[c], df_pred[c])
+    for c in extended_cols:
+        w = 0.5 / len(extended_cols)
+        final_score += w * balanced_accuracy_score(df_true[c], df_pred[c])
+    return final_score
 
